50dd8bf9ad075e6177f65cb5f56b003e27cb2516
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-02-18 02:56:35 +0200

    solve data

--- netlify/functions/events.js
+++ netlify/functions/events.js
@@ -22,23 +22,34 @@ exports.handler = async () => {
 
 	const $ = cheerio.load(html);
   let matches = [];
-$('.matchListContainer .matchAbridged').each((index, element) => {
-  
-  const team1NameAbb = $(element).find('.teamName abbr').eq(0).text().trim();
-  const team2NameAbb = $(element).find('.teamName abbr').eq(1).text().trim();
-  const team1Name = $(element).find('.teamName abbr').eq(0).attr('title');
-  const team2Name = $(element).find('.teamName abbr').eq(1).attr('title');
-  const team1Img = $(element).find('img').eq(0).attr('src');
-  const team2Img = $(element).find('img').eq(1).attr('src');
-  const matchScore = $(element).find('.score').text().trim() || "0-0";
-  const matchStartsAt = $(element).find('time').attr('data-kickoff') || 'end';
-  const broadcasterImage = $(element).find('.broadcaster-image').attr('src');
-  const minutes = $(element).find('.js-minutes').text().trim();
-  const isLive =  $(element).hasClass('js-live-fixture');
-  
-  const match = {minutes,isLive,broadcasterImage, team1NameAbb, team2NameAbb,team1Name, team2Name, team1Img, team2Img ,matchScore, matchStartsAt};
-  matches.push(match);
-});
+  const getData = (element) =>{
+    const team1NameAbb = $(element).find('.teamName abbr').eq(0).text().trim();
+    const team2NameAbb = $(element).find('.teamName abbr').eq(1).text().trim();
+    const team1Name = $(element).find('.teamName abbr').eq(0).attr('title');
+    const team2Name = $(element).find('.teamName abbr').eq(1).attr('title');
+    const team1Img = $(element).find('img').eq(0).attr('src');
+    const team2Img = $(element).find('img').eq(1).attr('src');
+    const matchScore = $(element).find('.score').text().trim() || "0-0";
+    const matchStartsAt = $(element).find('time').attr('data-kickoff') || 'end';
+    const broadcasterImage = $(element).find('.broadcaster-image').attr('src');
+    const minutes = $(element).find('.js-minutes').text().trim();
+    const isLive =  $(element).hasClass('js-live-fixture');
+    const match = {minutes,isLive,broadcasterImage, team1NameAbb, team2NameAbb,team1Name, team2Name, team1Img, team2Img ,matchScore, matchStartsAt};
+
+    return match
+  }
+
+  const htmlMatches = $('.matchListContainer .matchAbridged').filter('[href]');
+  if (htmlMatches.length > 1) {
+    $('.matchListContainer .matchAbridged').filter('[href]').each((index, element) => {
+      const match = getData(element);
+      matches.push(match);
+    });
+  }else{
+    const match = getData(htmlMatches);
+    matches.push(match);
+  }
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify({res:matches}),
