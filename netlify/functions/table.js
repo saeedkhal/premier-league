a02c2c6d069ae0e0c63d80a93a4de69a5517f138
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-02-19 22:11:24 +0200

    handel err

--- netlify/functions/table.js
+++ netlify/functions/table.js
@@ -2,62 +2,71 @@ const cheerio = require('cheerio');
 const request = require('request-promise');
 const fs = require('fs');
 exports.handler = async () => {
+  try {
 
-    // const html = await request.get('https://www.premierleague.com/tables');
-    // fs.writeFileSync('test.html', html)
 
-    const html = fs.readFileSync('test.html')
-      const $ = cheerio.load(html);
+    const html = await request.get('https://www.premierleague.com/tables');
+    fs.writeFileSync('test.html', html)
+
+    // const html = fs.readFileSync('test.html')
+    const $ = cheerio.load(html);
 
     let tables = [];
-  $('table').eq(0).find('tbody tr').not('.expandable').each((index, tr) => {
-    // table col
-     const position =  $(tr).find('.pos .value').text().trim();
-     const movment =  $(tr).find('.pos .movement').attr('class').split(' ').at(-1);
-     console.log(movment)
-     const teamLink =  $(tr).find('.team a').attr('href');
-     const teamAbb = $(tr).find('.team .short').text().trim();
-     const teamName = $(tr).find('.team .long').text().trim();
-     const played = $(tr).find('td').eq('3').text().trim();
-     const won = $(tr).find('td').eq('4').text().trim();
-     const drawn = $(tr).find('td').eq('5').text().trim();
-     const lost = $(tr).find('td').eq('6').text().trim();
-     const points = $(tr).find('td.points').text().trim();
+    $('table').eq(0).find('tbody tr').not('.expandable').each((index, tr) => {
+      // table col
+      const position = $(tr).find('.pos .value').text().trim();
+      const movment = $(tr).find('.pos .movement').attr('class').split(' ').at(-1);
+      console.log(movment)
+      const teamLink = $(tr).find('.team a').attr('href');
+      const teamAbb = $(tr).find('.team .short').text().trim();
+      const teamName = $(tr).find('.team .long').text().trim();
+      const played = $(tr).find('td').eq('3').text().trim();
+      const won = $(tr).find('td').eq('4').text().trim();
+      const drawn = $(tr).find('td').eq('5').text().trim();
+      const lost = $(tr).find('td').eq('6').text().trim();
+      const points = $(tr).find('td.points').text().trim();
 
-    //  fomAbb
-     const formAbb = $(tr).find('td.form li').map((index2,form) =>{
+      //  fomAbb
+      const formAbb = $(tr).find('td.form li').map((index2, form) => {
         const matchDteails = {};
         matchDteails.formAbbreviation = $(form).find('.form-abbreviation').text().trim();
-        matchDteails.matchLink =  $(form).find('.tooltip-link').attr('href');
-        matchDteails.matchInfo =  $(form).find('.tooltip-content .matchInfo').text().trim();
+        matchDteails.matchLink = $(form).find('.tooltip-link').attr('href');
+        matchDteails.matchInfo = $(form).find('.tooltip-content .matchInfo').text().trim();
         matchDteails.homeTeam = $(form).find('.tooltip-content .teamName').first().text().trim();
-        matchDteails.awayTeam =  $(form).find('.tooltip-content .teamName').last().text().trim();
-        matchDteails.score =  $(form).find('.tooltip-content .score').text().trim();
-        matchDteails.homeTeamImg =  $(form).find('.tooltip-content .badge-image').first().attr('src');
-        matchDteails.awayTeamImg =  $(form).find('.tooltip-content .badge-image').last().attr('src');
-        
-        return matchDteails;
-     }).get();
+        matchDteails.awayTeam = $(form).find('.tooltip-content .teamName').last().text().trim();
+        matchDteails.score = $(form).find('.tooltip-content .score').text().trim();
+        matchDteails.homeTeamImg = $(form).find('.tooltip-content .badge-image').first().attr('src');
+        matchDteails.awayTeamImg = $(form).find('.tooltip-content .badge-image').last().attr('src');
 
-// next match
-    const nextMatch = {};
-    nextMatch.badgeImage = $(tr).find('td.nextMatchCol .badge-image').attr('src');
-    nextMatch.matchLink =  $(tr).find('td.nextMatchCol .tooltip-link').attr('href');
-    nextMatch.matchInfo =  $(tr).find('td.nextMatchCol .tooltip-content .matchInfo').text().trim();
-    nextMatch.time =  $(tr).find('td.nextMatchCol .tooltip-content time').text().trim() || "0-0";
-    nextMatch.homeTeamAbbr =  $(tr).find('td.nextMatchCol .tooltip-content .teamName abbr').first().text().trim();
-    nextMatch.awayTeamAbbr =  $(tr).find('td.nextMatchCol .tooltip-content .teamName abbr').last().text().trim();
-    nextMatch.homeTeam=  $(tr).find('td.nextMatchCol .tooltip-content .teamName abbr').first().attr('title');
-    nextMatch.awayTeam =  $(tr).find('td.nextMatchCol .tooltip-content .teamName abbr').last().attr('title');
-    
-     tables.push({position,movment, teamLink, teamAbb, teamName, played, won, drawn, lost, points, formAbb, nextMatch});
+        return matchDteails;
+      }).get();
+
+      // next match
+      const nextMatch = {};
+      nextMatch.badgeImage = $(tr).find('td.nextMatchCol .badge-image').attr('src');
+      nextMatch.matchLink = $(tr).find('td.nextMatchCol .tooltip-link').attr('href');
+      nextMatch.matchInfo = $(tr).find('td.nextMatchCol .tooltip-content .matchInfo').text().trim();
+      nextMatch.time = $(tr).find('td.nextMatchCol .tooltip-content time').text().trim() || "0-0";
+      nextMatch.homeTeamAbbr = $(tr).find('td.nextMatchCol .tooltip-content .teamName abbr').first().text().trim();
+      nextMatch.awayTeamAbbr = $(tr).find('td.nextMatchCol .tooltip-content .teamName abbr').last().text().trim();
+      nextMatch.homeTeam = $(tr).find('td.nextMatchCol .tooltip-content .teamName abbr').first().attr('title');
+      nextMatch.awayTeam = $(tr).find('td.nextMatchCol .tooltip-content .teamName abbr').last().attr('title');
+
+      tables.push({ position, movment, teamLink, teamAbb, teamName, played, won, drawn, lost, points, formAbb, nextMatch });
 
     });
-	return {
-		statusCode: 200,
-		body: JSON.stringify({res:tables}),
-    headers: {
-      "Content-Type": "application/json"
-    },
-	}
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ res: tables }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    }
+  }
+  catch (err) {
+    return {
+      status: 500,
+      body: JSON.stringify('internal server error')
+    }
+  }
 }
